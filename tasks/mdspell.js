3f3bbe2c7af8186b43c62b2940582006f61a6289
redaxmedia <info@redaxmedia.com>
2020-07-12 23:03:57 +0200

    Fix spell checker

--- tasks/mdspell.js
+++ tasks/mdspell.js
@@ -10,8 +10,7 @@ module.exports = () =>
 				[
 					'documentation/**/*.md',
 					'!documentation/03.languages/02.language-codes.md',
-					'!documentation/06.modules/02.module-overview.md',
-					'!documentation/06.modules/04.module-hooks.md'
+					'!documentation/06.modules/03.module-hooks.md'
 				]
 			},
 			options:
